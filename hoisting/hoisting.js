1f0cfabcaea18649d3902cb26c7d740e22384ab3
poojadayal <poojaadayal@gmail.com>
2024-09-26 20:48:43 +0530

    function hoisting
    Signed-off-by: poojadayal <poojaadayal@gmail.com>

--- hoisting/hoisting.js
+++ hoisting/hoisting.js
@@ -1,3 +1,25 @@
+/*
+    hoisting:hoisting is javascript mechanism 
+    where variable and function declaration are moved
+    to the top of scopd during complie phase,only function
+    declaration is hoisted 
+*/
+
+/*
+ hoisting for variable :
+     -variable declare with 'var' keyword are hoisted and initalized with 'undefined'
+     -variable declare with 'let' and 'const' keyword are hoisted but not initalized due to temporal dead zone
+*/
+
+/*
+hoisting for function
+ function declaration are fully hoisted ,meaning the entire function can be called before is declaration in the code
+ function expression ,those assigned to 'var','let' and 'const' are not hoisted in same way only variable declaration
+ is hoisted ,not the function itself
+
+
+*/
+
 
 console.log('value of a is ', a);   //undefined
 var a = 10;
